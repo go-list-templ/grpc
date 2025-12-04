04ae250eefeea5ea2f747ce453938432d04b935d
Denis <korbakovd@gmail.com>
2025-12-04 18:52:43 +0300

    fix: update fields

--- internal/domain/model.go
+++ internal/domain/model.go
@@ -4,7 +4,8 @@ import "time"
 
 type User struct {
 	ID        int64     `json:"id"`
-	Username  string    `json:"username"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
