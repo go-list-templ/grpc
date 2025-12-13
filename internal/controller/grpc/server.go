b36089e2720d0bdcbc9d339110541305198b41aa
Denis <korbakovd@gmail.com>
2025-12-13 15:05:01 +0300

    feat: add logic grpc server

--- internal/controller/grpc/server.go
+++ internal/controller/grpc/server.go
@@ -1,1 +1,44 @@
 package grpc
+
+import (
+	"net"
+
+	"github.com/go-list-templ/grpc/config"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	server *grpc.Server
+	config *config.Server
+	errors chan error
+}
+
+func NewServer(cfg *config.Server) *Server {
+	grpcServer := grpc.NewServer()
+
+	return &Server{
+		server: grpcServer,
+		config: cfg,
+		errors: make(chan error, 1),
+	}
+}
+
+func (s *Server) Notify() <-chan error {
+	return s.errors
+}
+
+func (s *Server) Start() {
+	go func() {
+		lis, err := net.Listen("tcp", net.JoinHostPort("", s.config.GRPCPort))
+		if err != nil {
+			s.errors <- err
+		}
+
+		s.errors <- s.server.Serve(lis)
+		close(s.errors)
+	}()
+}
+
+func (s *Server) Stop() {
+	s.server.GracefulStop()
+}
